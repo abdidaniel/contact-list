5231ea0a8deb06b3561e89852fb57a4311ed4991
ademx7 <abdidaniel2000@gmail.com>
2023-04-30 23:11:05 -0500

    Lista de contactos en js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-let contactos = ["Juan Castro", "Maria Rodriguez", "Lis Garcís"];
+let contactos = ["Juan Castro", "Maria Rodriguez", "Luis Garcís"];
 
 function agregarContacto(nombre) {
     contactos.push(nombre);
-    console.log("${nombre} ha sido agregado.");
+    console.log(nombre + " ha sido agregado.");
+  }
+
+  function eliminarContacto(nombre) {
+    const val = contactos.indexOf(nombre);
+    if (val !== -1) {
+      contactos.splice(val, 1);
+      console.log(nombre + " ha sido eliminado.");
+    } else {
+      console.log($nombre + " no existe en la lista.");
+    }
+  }
+
+  function imprimirContactos() {
+    for (let i = 0; i < contactos.length; i++) {
+      console.log(contactos[i]);
+    }
   }
