3a782ba71808b444f07a9e08deb04f2e0a60f132
ademx7 <abdidaniel2000@gmail.com>
2023-04-30 22:51:26 -0500

    Creando repositorio

---
+++ script.js
@@ -0,0 +1,6 @@
+let contactos = ["Juan Castro", "Maria Rodriguez", "Lis Garcís"];
+
+function agregarContacto(nombre) {
+    contactos.push(nombre);
+    console.log("${nombre} ha sido agregado.");
+  }
